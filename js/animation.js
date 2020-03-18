c35986d3bf2d6f7490c74e3e24154b81397e5a16
ronanren <renouxpro@gmail.com>
2020-03-18 22:57:56 +0100

    Update animation

--- js/animation.js
+++ js/animation.js
@@ -1,8 +1,10 @@
+window.onload = () => scroll(0, 1);
 $(window).scroll(function() {
   var scrolledFromTop = $(window).scrollTop() + $(window).height();
+  console.log(scrolledFromTop);
   $(".appear").each(function() {
     var distanceFromTop = $(this).offset().top;
-    if (scrolledFromTop >= distanceFromTop + 50) {
+    if (scrolledFromTop >= distanceFromTop) {
       var delaiAnim = $(this).data("delai");
       $(this)
         .delay(delaiAnim)
