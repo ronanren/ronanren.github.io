258b00d694adcce2a4a863d72e21539263f20ee3
ronanren <renouxpro@gmail.com>
2020-03-23 18:08:03 +0100

    Update

--- js/app.js
+++ js/app.js
@@ -6,44 +6,6 @@ for (i = 0; i < bars.length; i++) {
   bars[i].textContent = "";
 }
 
-/* DARK MODE */
-const toggleSwitch = document.querySelector('.checkbox input[type="checkbox"]');
-let currentTheme = "light";
-
-try {
-  currentTheme = localStorage.getItem("theme");
-} catch (e) {
-  currentTheme = "light";
-}
-
-if (currentTheme) {
-  document.documentElement.setAttribute("data-theme", currentTheme);
-
-  if (currentTheme === "dark") {
-    toggleSwitch.checked = true;
-  }
-}
-
-function switchTheme(e) {
-  if (e.target.checked) {
-    document.documentElement.setAttribute("data-theme", "dark");
-    try {
-      localStorage.setItem("theme", "dark");
-    } catch (e) {
-      console.log("Localstorage impossible");
-    }
-  } else {
-    document.documentElement.setAttribute("data-theme", "light");
-    try {
-      localStorage.setItem("theme", "light");
-    } catch (e) {
-      console.log("Localstorage impossible");
-    }
-  }
-}
-
-toggleSwitch.addEventListener("change", switchTheme, false);
-
 /* BOUTON BACK TO TOP */
 
 $(document).ready(function() {
